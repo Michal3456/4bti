321417c2f2f6a5ab2dd8d6de711c769776ce5132
kdola <94521278+kdola@users.noreply.github.com>
2021-12-03 09:06:21 +0100

    Delete build directory

--- build/static/js/main.4b339afd.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpprojekt_bazy=this.webpackJsonpprojekt_bazy||[]).push([[0],{9:function(t,e,n){"use strict";n.r(e);var r=n(1),c=n.n(r),o=n(3),i=n.n(o),u=n(0);var s=function(){return Object(u.jsxs)("form",{id:"dataForm",onSubmit:function(t){t.preventDefault()},children:[Object(u.jsx)("input",{type:"text",placeholder:"Input your device name here."}),Object(u.jsx)("button",{type:"submit",children:"Submit"})]})};i.a.render(Object(u.jsx)(c.a.StrictMode,{children:Object(u.jsx)(s,{})}),document.getElementById("root"))}},[[9,1,2]]]);
-//# sourceMappingURL=main.4b339afd.chunk.js.map
