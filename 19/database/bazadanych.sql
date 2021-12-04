8010babd57306231a4cedc9d0a0e50f3b387cfee
Mateusz Ż <51126753+godryk75@users.noreply.github.com>
2021-12-04 19:13:06 +0100

    Add files via upload

---
+++ 19/database/bazadanych.sql
@@ -0,0 +1,88 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 04 Gru 2021, 19:11
+-- Wersja serwera: 10.4.21-MariaDB
+-- Wersja PHP: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `praca2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `czas`
+--
+
+CREATE TABLE `czas` (
+  `ID` int(255) NOT NULL,
+  `user_id` varchar(255) COLLATE utf8_polish_ci NOT NULL,
+  `dzien` varchar(255) COLLATE utf8_polish_ci NOT NULL,
+  `start` varchar(255) COLLATE utf8_polish_ci NOT NULL,
+  `stop` varchar(255) COLLATE utf8_polish_ci NOT NULL,
+  `razem` varchar(255) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `users`
+--
+
+CREATE TABLE `users` (
+  `ID` int(11) NOT NULL,
+  `user_id` varchar(255) COLLATE utf8_polish_ci NOT NULL,
+  `imie` varchar(255) COLLATE utf8_polish_ci NOT NULL,
+  `nazwisko` varchar(255) COLLATE utf8_polish_ci NOT NULL,
+  `haslo` varchar(255) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `czas`
+--
+ALTER TABLE `czas`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indeksy dla tabeli `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT dla zrzuconych tabel
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `czas`
+--
+ALTER TABLE `czas`
+  MODIFY `ID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT dla tabeli `users`
+--
+ALTER TABLE `users`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
