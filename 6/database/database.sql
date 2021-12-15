b4f58635cd64c2d9427b987d16f743f29ac00172
Piotr <72739034+pn073tib@users.noreply.github.com>
2021-12-15 16:54:54 +0100

    Update database.sql

--- 6/database/database.sql
+++ 6/database/database.sql
@@ -1,1 +1,108 @@
 
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 01 Gru 2021, 15:15
+-- Wersja serwera: 10.4.19-MariaDB
+-- Wersja PHP: 8.0.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `bd1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `rezerwation`
+--
+
+CREATE TABLE `rezerwation` (
+  `IdRezerwation` int(255) NOT NULL,
+  `IdUser` int(255) NOT NULL,
+  `IdRoom` int(255) NOT NULL,
+  `Date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `room`
+--
+
+CREATE TABLE `room` (
+  `IdRoom` int(255) NOT NULL,
+  `Adress` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `user`
+--
+
+CREATE TABLE `user` (
+  `IdUser` int(255) NOT NULL,
+  `FirstName` varchar(300) NOT NULL,
+  `LaseName` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `rezerwation`
+--
+ALTER TABLE `rezerwation`
+  ADD PRIMARY KEY (`IdRezerwation`);
+
+--
+-- Indeksy dla tabeli `room`
+--
+ALTER TABLE `room`
+  ADD PRIMARY KEY (`IdRoom`);
+
+--
+-- Indeksy dla tabeli `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`IdUser`);
+
+--
+-- AUTO_INCREMENT dla zrzuconych tabel
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `rezerwation`
+--
+ALTER TABLE `rezerwation`
+  MODIFY `IdRezerwation` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `room`
+--
+ALTER TABLE `room`
+  MODIFY `IdRoom` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT dla tabeli `user`
+--
+ALTER TABLE `user`
+  MODIFY `IdUser` int(255) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
