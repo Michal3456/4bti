b33dc022635f0d10588d9ee0abfd58ed99b0be7a
Piotr <72739034+pn073tib@users.noreply.github.com>
2021-12-20 09:39:15 +0100

    Update skrypt.php

--- 6/add/skrypt.php
+++ 6/add/skrypt.php
@@ -1,1 +1,17 @@
+<?php
+		if(isset($_POST['wyślij']))
+        {
+        	$first_name = $_POST['first_name'];
+			$last_name = $_POST['last_name'];
+            $room = $_POST['room'];
+            $date_book = $_POST['date_booking'];
 
+			$pol = new Mysqli('localhost','root','','reservation');
+            
+            $sql = "INSERT INTO reservation1 (first_name, last_name, room, date_booking) values('$first_name', '$last_name', '$room', '$date_book')";
+            
+           	$pol -> query($sql);
+            
+            $pol -> close();
+
+?>
