30d4e27b96d7bfdc7dbd34554fe605adf0328edf
Piotr <72739034+pn073tib@users.noreply.github.com>
2021-12-21 08:16:03 +0100

    Update view0.php

--- 6/view/view0.php
+++ 6/view/view0.php
@@ -1,1 +1,21 @@
+	<table>
 
+	<th> First name </th> <th> Last name </th> <th> room </th> <th> Date </th>
+
+<?php
+
+			$pol = new Mysqli('localhost','root','','reservation');
+            
+            $sql = "SELECT * FROM reservation1";
+            
+            $wynik = $pol -> query($sql);
+            while($w = $wynik -> fetch_row());
+            {
+            	echo "<tr> <td> $w[0] </td> <td> $w[1] </td> <td> $w[2] </td> <td> $w[3] </td> </tr>"
+            }
+           
+            $pol -> close();
+
+?>
+
+	</table>
