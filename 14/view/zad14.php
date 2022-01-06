5a101ad96167f274f795dc7f3692f058885d6a6c
Mariusz Tyczyński <56454095+mtyczynski27@users.noreply.github.com>
2022-01-06 19:40:14 +0100

    Add files via upload

---
+++ 14/view/zad14.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>view0</title>
+    <style>
+        table, tr, td, th
+        {
+            border: solid black 1px;
+        }
+    </style>
+    <script>
+        function ile()
+        {
+            let ilosc = document.getElementsByClassName("klasa").length;
+            alert(ilosc);
+        }
+    </script>
+</head>
+<body>
+    <table>
+        <thead>
+            <th> id </th>
+            <th> name </th>
+            <th> link </th>
+        </thead>
+        <tbody>
+            <?php
+                $pol = new Mysqli('localhost', 'root', '', 'self_learning_system');
+                $sql = "SELECT `id`, `name`, `link` FROM images";
+                $wynik = $pol -> query($sql);
+                while($w=$wynik->fetch_row())
+                    echo "<tr> <td class='klasa'> $w[0] </td> <td> $w[1] </td> <td> $w[2] </td> </tr>";
+                $pol->close();
+            ?>
+        </tbody>
+    </table>
+    <button onclick="ile()">ile</button>
+</body>
+</html>
